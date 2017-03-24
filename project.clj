f6052060a6de4150e042ffb129ee2ccee08bee13
Daniel Berg <mail@roosta.sh>
2017-03-24 21:56:41 +0100

    Init project struct

--- project.clj
+++ project.clj
@@ -3,7 +3,25 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot startpage.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [org.clojure/clojurescript "1.9.473"]]
+
+  :plugins [[lein-cljsbuild "1.1.5"]]
+
+  :clean-targets ^{:protect false} ["resources/public/js" "target"]
+
+  :cljsbuild {:builds [{:id "min"
+                        :source-paths ["src/cljs"]
+                        :compiler {:main "startpage.core"
+                                   :output-to "resources/public/js/app.js"
+                                   :verbose true
+                                   :pretty-print false
+                                   :optimizations :advanced
+                                   :closure-defines {"goog.DEBUG" false}}}]}
+
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.10"]
+                                  [figwheel-sidecar "0.5.9"]
+                                  [binaryage/devtools "0.9.1"]]
+                   :plugins [[lein-figwheel "0.5.9"]]
+                   :source-paths ["dev" "script"]}})
