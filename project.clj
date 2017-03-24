191f355952f93aefd5263d366be52a041580565b
Daniel Berg <mail@roosta.sh>
2017-03-24 22:16:38 +0100

    Rollback deps: compilation error of core.async

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
-                 [org.clojure/clojurescript "1.9.495"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [org.clojure/clojurescript "1.9.473"]]
 
   :plugins [[lein-cljsbuild "1.1.6-SNAPSHOT"]]
 
