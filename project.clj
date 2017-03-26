9319175fd0fe5032692454a4c67a19cc78f04e32
Daniel Berg <mail@roosta.sh>
2017-03-26 23:27:12 +0200

    Fix build issues

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                 [org.clojure/clojurescript "1.9.473"]]
+                 [org.clojure/clojurescript "1.9.473"]
+                 [reagent "0.6.1"]
+                 [cljs-jss "6.5.0-0"]]
 
   :plugins [[lein-cljsbuild "1.1.6-SNAPSHOT"]]
 
