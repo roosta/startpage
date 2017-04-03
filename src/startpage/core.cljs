6b7cd22daa8b921b6157ba0c019758ec79733772
Daniel Berg <mail@roosta.sh>
2017-04-03 22:09:12 +0200

    Add clock, palette

--- src/startpage/core.cljs
+++ src/startpage/core.cljs
@@ -2,15 +2,39 @@
   (:require
    [cljsjs.react-jss]
    [goog.object :as gobj]
+   [startpage.srcery :refer [colors]]
    [reagent.core :as r]
    [reagent.debug :as d]))
 
-(def styles {"@global" {:body {:background-color "blue"}}
-             :asd {:color "white"
-                   :background-color "black"}})
+(def styles {"@global" {:body {:margin 0
+                               ;; :font-family "'Ubuntu Mono', monospaced"
+                               ;; :font-family "'VT323', monospaced"
+                               :font-family "'Lato Thin', sans-serif"
+                               :background-color (-> colors :black :hex)
+                               }}
+             :root {:color "white"
+                    :display "flex"
+                    :height "70vh"
+                    :align-items "center"
+                    :justify-content "center"}
+
+             :clock {:font-size "10em"
+                     :border-bottom (str "0.02em solid " (-> colors :white :hex))
+                     :color (-> colors :white :hex)}})
 
 (defonce inject-sheet (.create js/reactJss))
 (def style-wrapper (inject-sheet (clj->js styles)))
+
+(defonce timer (r/atom (js/Date.)))
+(defonce time-updater (js/setInterval
+                       #(reset! timer (js/Date.)) 1000))
+
+(defn clock
+  [classnames]
+  (let [time-str (-> @timer .toTimeString (clojure.string/split " ") first)]
+    [:div
+     {:class (gobj/get classnames "clock")}
+     time-str]))
 
 (defn startpage
   []
@@ -18,8 +42,8 @@
    {:reagent-render
     (fn []
       (let [classnames (:classes (r/props (r/current-component)))]
-        [:div {:class (gobj/get classnames "asd")}
-         "asdadasd"])
+        [:div {:class (gobj/get classnames "root")}
+         [clock classnames]])
       )}))
 
 (def app (r/adapt-react-class (style-wrapper (startpage))))
---
+++ src/startpage/srcery.cljs
@@ -0,0 +1,52 @@
+(ns startpage.srcery)
+
+(def colors {:black          {:hex  "#1C1B19"
+                              :rgb  [28,  27,  25]
+                              :code 0}
+             :red            {:hex  "#FF3128"
+                              :rgb  [255, 49,  40]
+                              :code 1}
+             :green          {:hex  "#519F50"
+                              :rgb  [81,  159, 80]
+                              :code 2}
+             :yellow         {:hex  "#FBB829"
+                              :rgb  [251, 184, 41]
+                              :code 3}
+             :blue           {:hex  "#5573A3"
+                              :rgb  [85,  115, 163]
+                              :code 4}
+             :magenta        {:hex  "#E02C6D"
+                              :rgb  [224, 44,  109]
+                              :code 5}
+             :cyan           {:hex  "#0AAEB3"
+                              :rgb  [10, 174, 179]
+                              :code 6}
+             :grey           {:hex  "#918175"
+                              :alt  "white"
+                              :rgb  [145, 129, 117]
+                              :code 7 }
+             :bright-black   {:hex  "#2D2B28"
+                              :rgb  [45,  43,  40]
+                              :code 8}
+             :bright-red     {:hex  "#F75341"
+                              :rgb  [247, 83, 65]
+                              :code 9}
+             :bright-green   {:hex  "#F75341"
+                              :rgb  [247, 83, 65]
+                              :code 10}
+             :bright-yellow  {:hex  "#FED06E"
+                              :rgb  [254, 208, 110]
+                              :code 11}
+             :bright-blue    {:hex  "#8EB2F7"
+                              :rgb  [142, 178, 247]
+                              :code 12}
+             :bright-magenta {:hex  "#E35682"
+                              :rgb  [227, 86,  130]
+                              :code 13}
+             :bright-cyan    {:hex  "#53FDE9"
+                              :rgb  [83, 253, 233]
+                              :code 14}
+             :white          {:hex  "#FCE8C3"
+                              :alt  "bright-white"
+                              :rgb  [252, 232, 195]
+                              :code 15}})
