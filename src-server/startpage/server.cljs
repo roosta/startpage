6ce7dcebbf35fd0e7e1616d3513d3604ff9226d4
Daniel Berg <mail@roosta.sh>
2017-04-15 19:26:57 +0200

    Add a post endpoint for figlet
    passing in a json object with a text prop and a string value

--- src-server/startpage/server.cljs
+++ src-server/startpage/server.cljs
@@ -7,23 +7,25 @@
 (def express (nodejs/require "express"))
 (def serve-static (nodejs/require "serve-static"))
 (def figlet (nodejs/require "figlet"))
+(def body-parser (nodejs/require "body-parser"))
 
 (defn handle-request [req res]
   (.send res (tools/render-page (.-path req))))
 
 (defn handle-figlet
   [req res]
-  (let [input (.. req -params -input)]
-    (figlet input "Fraktur" (fn [err text]
+  (let [text (.. req -body -text)]
+    (figlet text "Fraktur" (fn [err text]
                               (when err
                                 (.log js/console "something went wrong")
                                 (.dir js/console err))
                               (.send res text)))))
 
 (defn -main []
-  (let [app (express)]
+  (let [app (express)
+        json-parser (.json body-parser)]
     (.get app "/" handle-request)
-    (.get app "/figlet/:input" handle-figlet)
+    (.post app "/figlet/" json-parser handle-figlet)
     (.use app (serve-static "resources/public/js"))
     (.listen app 3000 (fn []
                         (println "Server started on port 3000")))))
