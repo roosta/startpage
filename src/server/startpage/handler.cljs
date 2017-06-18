1a06640462cabb56ec8cbfebdf326955e57fffdd
Daniel Berg <mail@roosta.sh>
2017-06-18 20:34:02 +0200

    Set title

--- src/server/startpage/handler.cljs
+++ src/server/startpage/handler.cljs
@@ -10,6 +10,7 @@
   [:html
    [:head
     [:meta {:charset "utf-8"}]
+    [:title "Startpage"]
     [:meta {:name    "viewport"
             :content "width=device-width, initial-scale=1.0"}]
     [:link {:href "https://fonts.googleapis.com/css?family=Inconsolata" :rel "stylesheet"}]]
