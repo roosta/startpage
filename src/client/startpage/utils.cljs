adc8fea5a00ecc05261a92e5354c09e207e93775
Daniel Berg <mail@roosta.sh>
2017-07-05 13:55:26 +0200

    Update k-style-number to include single decimal

--- src/client/startpage/utils.cljs
+++ src/client/startpage/utils.cljs
@@ -7,12 +7,15 @@
     field))
 
 (defn join-classes
+  "take variable number of map keys and return classnames joined as a string "
   [styles & classes]
   (->> (select-keys styles classes)
        vals
        (clojure.string/join " ")))
 
 (defn transition
+  "take args and return a transtion string
+  Needs work"
   [{:keys [prop duration timing-fn delay]
     :or {prop "all"
          duration "0s"
@@ -25,7 +28,8 @@
   e.g: 1000 = 1k"
   [number]
   (if (> number 999)
-    (if (= (mod (/ number 100) 10) 0)
+    (str (.toFixed (/ (/ number 100) 10.0) 1) "k")
+    #_(if (= (mod (/ number 100) 10) 0)
       (str (.floor js/Math (/ number 1000)) "k")
       (str (.floor js/Math (/ (/ number 100) 10.0)) "k"))
     number))
